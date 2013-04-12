1d975e5f6be594d9fe12492e6e9fa36e81807975
Frank Lee <frankguolee@gmail.com>
2013-04-11 22:25:44 -0400

    Revert "Merge pull request #6 from dpiao92/master"
    This reverts commit e23bf669eebc697283805f652c7bf73b4c475900, reversing
    changes made to 48e31ff5a6c418e1256a979627f5037cbecaa980.

--- LCD_tut/Switch_case/Switch_case.ino
+++ LCD_tut/Switch_case/Switch_case.ino
@@ -7,15 +7,6 @@ Mark Bramwell, July 2010
 // select the pins used on the LCD panel
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 // define some values used by the panel and buttons
-
-// Timer Variables
-int minutes;
-int seconds;
-boolean select = true;
-
-// Variables
-int buttonPin = A0;
-
 int lcd_key = 0;
 int adc_key_in = 0;
 #define btnRIGHT 0
@@ -24,146 +15,63 @@ int adc_key_in = 0;
 #define btnLEFT 3
 #define btnSELECT 4
 #define btnNONE 5
-#define V1 20
-#define V2 119
-#define V3 276
-#define V4 430
-#define V5 660
-#define VNONE 1003
+#define V1 500
+#define V2 500
+#define V3 500
+#define V4 500
+#define V5 500
+#define VNONE 500
 // read the buttons
 int read_LCD_buttons()
 {
-adc_key_in = debounce(adc_key_in); // read the value from the sensor
-if (adc_key_in > VNONE) return btnNONE; // We make this the 1st option for speed reasons since 
-if (adc_key_in < V1) return btnRIGHT;
+adc_key_in = analogRead(0); // read the value from the sensor
+// my buttons when read are centered at these valies: 0, 144, 329, 504, 741
+// we add approx 50 to those values and check to see if we are close
+if (adc_key_in > VNONE) return btnNONE; // We make this the 1st option for speed reasons since if (adc_key_in < V1) return btnRIGHT;
 if (adc_key_in < V2) return btnUP;
 if (adc_key_in < V3) return btnDOWN;
 if (adc_key_in < V4) return btnLEFT;
 if (adc_key_in < V5) return btnSELECT;
 return btnNONE; // when all others fail, return this...
 }
-
-// Improved reading of button state
-int debounce(int last)
-{
-  int current = analogRead(buttonPin); // Current button state
-  if (last != current) // If a change happened
-  {
-    delay(10); // wait for bounce to settle
-    current = analogRead(buttonPin); 
-  }
-  return current;
-}
-
 void setup()
 {
 lcd.begin(16, 2); // start the library
-Serial.begin(9600);
-// Set up the timer
-
-
 }
-
-
 void loop()
 {
-lcd.print("Set Timer for Minutes");
-lcd.setCursor(0,1);
-lcd.blink();
-while (select == true) {
-  lcd_key = read_LCD_buttons(); // read the buttons
-  
-  switch (lcd_key) // depending on which button was pushed, we perform an action
-  {
-    case btnRIGHT:
-    {
-      //Instructions for what to do on RIGHT button press
-      break;
-    }
-    case btnLEFT:
-    {
-      //Instructions for what to do on LEFT button press
-      break;
-    }
-    case btnUP:
-    {
-      //Instructions for what to do on UP button press
-      minutes = minutes + 1;
-      lcd.setCursor(0,1);
-      lcd.print(minutes);
-      break;
-    }
-    case btnDOWN:
-    {
-      //Instructions for what to do on DOWN button press
-      if (minutes > 0) {
-        minutes = minutes-1;
-        lcd.setCursor(0,1);
-        lcd.print(minutes);
-      }
-      break;
-    }
-    case btnSELECT:
-    {
-      //Instructions for what to do on SELECT button press
-      break;
-    }
-    case btnNONE:
-    {
-      //Instructions for what to do if no button is pressed.
-      break;
-    }
-  }
-}
-
-lcd.clear();
-lcd.print("Set Timer for Seconds: ");
-lcd.setCursor(0,1);
-lcd.blink();
 lcd_key = read_LCD_buttons(); // read the buttons
-
 switch (lcd_key) // depending on which button was pushed, we perform an action
 {
-  case btnRIGHT:
-  {
-    //Instructions for what to do on RIGHT button press
-    break;
-  }
-  case btnLEFT:
-  {
-    //Instructions for what to do on LEFT button press
-    break;
-  }
-  case btnUP:
-  {
-    //Instructions for what to do on UP button press
-    if (seconds < 60) {
-    seconds = seconds + 1;
-    lcd.setCursor(0,1);
-    lcd.print(seconds);
-    }
-  }
-  case btnDOWN:
-  {
-    //Instructions for what to do on DOWN button press
-    if (seconds > 0) {
-      seconds = seconds - 1;
-      lcd.setCursor(0,1);
-      lcd.print(seconds);
-    }
-  }
-  case btnSELECT:
-  {
-    //Instructions for what to do on SELECT button press
-    break;
-  }
-  case btnNONE:
-  {
-    //Instructions for what to do if no button is pressed.
-    // do nothing
-  }
+case btnRIGHT:
+{
+//Instructions for what to do on RIGHT button press
+break;
 }
-
+case btnLEFT:
+{
+//Instructions for what to do on LEFT button press
+break;
 }
-
-
+case btnUP:
+{
+//Instructions for what to do on UP button press
+break;
+}
+case btnDOWN:
+{
+//Instructions for what to do on DOWN button press
+break;
+}
+case btnSELECT:
+{
+//Instructions for what to do on SELECT button press
+break;
+}
+case btnNONE:
+{
+//Instructions for what to do if no button is pressed.
+break;
+}
+}
+}
