6243cddab3a9b8af31e6df49ad06ad569d0f3fbb
Daniel Piao <dpiao92@gmail.com>
2013-05-02 13:57:03 -0400

    made timekeeper function

--- PID_Controller_2/LCD_output.ino
+++ PID_Controller_2/LCD_output.ino
@@ -43,3 +43,35 @@ void heating_print(){
   lcd.setCursor(8,1);
   lcd.print(millis());
 }
+
+void timekeeper(){
+  lcd.setCursor(8,1);
+   
+   current_time = millis();
+   print_time(millis() - current_time); 
+}
+
+// argument is time in milliseconds
+void print_time(unsigned long t_milli)
+{
+    char timer[20];
+    int mins;
+    int secs;
+    int fractime;
+    unsigned long inttime;
+
+    inttime  = t_milli / 1000; // inttime is the total number of number of seconds
+    fractime = t_milli % 1000; // fractimeis the number of thousandths of a second
+    
+    mins     = inttime / 60;
+    inttime  = inttime % 60;
+
+    // Now inttime is the number of seconds left after subtracting the number
+    // in the number of minutes. In other words, it is the number of seconds.
+    
+    secs = inttime;
+
+    // Don't bother to print days
+    sprintf(timer, "%02d:%02d.%03d", mins, secs, fractime);
+    lcd.print(timer);
+}
